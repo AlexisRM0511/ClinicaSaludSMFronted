73378b7b41fc49afaceb7cd952bc85fa8297e4ed
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-07-26 22:59:23 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'citas',
+    loadChildren: () => import('./cita/cita.module').then((m) => m.CitaModule),
+  },
+  {
+    path: 'doctor',
+    loadChildren: () =>
+      import('./doctor/doctor.module').then((m) => m.DoctorModule),
+  },
+  {
+    path: 'paciente',
+    loadChildren: () =>
+      import('./paciente/paciente.module').then((m) => m.PacienteModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/cita/cita.module.ts
+++ src/app/cita/cita.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CitaRoutingModule } from './cita-routing.module';
+import { RegistroCitasComponent } from './components/registro-citas/registro-citas.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegistroCitasComponent
+  ],
   imports: [
     CommonModule,
     CitaRoutingModule
