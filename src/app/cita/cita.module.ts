7d0aeb50cbf70a41aa95a691e7bfb92a1ca23a20
Alexander Marcos <marcos.valdez@hotmail.com>
2021-07-26 23:00:28 -0500

    componente crear cita

--- src/app/cita/cita.module.ts
+++ src/app/cita/cita.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CitaRoutingModule } from './cita-routing.module';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrearCitaComponent
+  ],
   imports: [
     CommonModule,
     CitaRoutingModule
