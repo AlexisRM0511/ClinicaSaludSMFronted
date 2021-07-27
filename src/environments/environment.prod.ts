5d9ccddfc43badd708733bcaaab768947fe80e19
Alexander Marcos <marcos.valdez@hotmail.com>
2021-07-27 13:39:22 -0500

    credenciasles de firebase en prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAy9kto9LXsOTlOMQrqS3RcLcaOIgmuOok",
+    authDomain: "clinicasaludsm.firebaseapp.com",
+    projectId: "clinicasaludsm",
+    storageBucket: "clinicasaludsm.appspot.com",
+    messagingSenderId: "51819425453",
+    appId: "1:51819425453:web:c74f405f252b174a93a024",
+    measurementId: "G-CV23SGPV6B"
+  }
 };
