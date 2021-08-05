da25b52c4c0f3143994538a6340cc266442264b1
Alexander Marcos <marcos.valdez@hotmail.com>
2021-08-05 15:11:27 -0500

    emergencia terminada

--- src/app/paciente/components/paciente-emergencia-registrado/paciente-emergencia-registrado.component.ts
+++ src/app/paciente/components/paciente-emergencia-registrado/paciente-emergencia-registrado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GenerarExcelService } from 'src/app/services/generar-excel.service';
+import Swal from 'sweetalert2';
 import { Emergencia } from '../../modal/emergencia';
 import { PacienteService } from '../../services/paciente.service';
 
@@ -15,20 +17,46 @@ export class PacienteEmergenciaRegistradoComponent implements OnInit {
   responsive: boolean = true;
   items = 5;
   emergencia$ = this.emergenciaService.emergencia;
-
+  emergencia: Emergencia[];
   emergenciaFilter='';
 
   constructor(
-    private emergenciaService: PacienteService
+    private emergenciaService: PacienteService,
+    private excelService: GenerarExcelService
   ) {}
 
   ngOnInit(): void {
     this.emergencia$;
     console.log(this.emergencia$);
-    
+    this.listarCitas();
+  }
+
+  listarCitas() {
+    this.emergencia$.subscribe((val) => {
+      this.emergencia = val;
+    });
   }
 
   elementosSeleccionados(valor) {
     this.items = valor.target.value;
+  }
+  descargarExcel(){
+    this.excelService.exportAsExcelFile(this.emergencia, 'Emergencia');
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 1000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Descargando...',
+    });
   }
 }
