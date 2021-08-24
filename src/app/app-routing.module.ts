284a5a6c0011521bf88ec67e6e706abdc75d4771
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-24 01:47:22 -0500

    feat: Restringir seleccion de fecha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile',
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
   },
