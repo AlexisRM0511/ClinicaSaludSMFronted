956b994690050f4b37c7211aa1e5cf3454adc15c
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-24 20:16:14 -0500

    fix: overflow forms editar

--- src/app/cita/components/editar-cita/editar-cita.component.ts
+++ src/app/cita/components/editar-cita/editar-cita.component.ts
@@ -7,57 +7,61 @@ import { CitaService } from '../../services/cita.service';
 @Component({
   selector: 'app-editar-cita',
   templateUrl: './editar-cita.component.html',
-  styleUrls: ['./editar-cita.component.css']
+  styleUrls: ['./editar-cita.component.css'],
 })
 export class EditarCitaComponent implements OnInit {
-
   cita: Cita;
   citaForm: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder, private citaService: CitaService) { 
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private citaService: CitaService
+  ) {
     const navigation = this.router.getCurrentNavigation();
     this.cita = navigation?.extras?.state?.value;
     this.initForm();
   }
 
   ngOnInit(): void {
-    if (typeof this.cita === 'undefined'){
+    if (typeof this.cita === 'undefined') {
       this.router.navigate(['/citas/crear']);
-    }else{
+    } else {
       this.citaForm.patchValue(this.cita);
     }
   }
 
-  onSave(): void{
+  onSave(): void {
     console.log('Saved', this.citaForm.value);
-    if (this.citaForm.valid){
+    if (this.citaForm.valid) {
       const cita = this.citaForm.value;
       const citaId = this.cita?.id || null;
-      this.citaService.onSaveCita(cita, citaId)
+      this.citaService.onSaveCita(cita, citaId);
       this.citaForm.reset();
     }
   }
 
-  onGoBackToList():void{
+  onGoBackToList(): void {
     this.router.navigate(['/citas/registro-citas']);
   }
 
-  isValidField(field: string):string{
+  isValidField(field: string): string {
     const validatedField = this.citaForm.get(field);
-    return ( !validatedField.valid && validatedField.touched)
-      ? 'is-invalid' : validatedField.touched ? 'is-valid' : '';
+    return !validatedField.valid && validatedField.touched
+      ? 'is-invalid'
+      : validatedField.touched
+      ? 'is-valid'
+      : '';
   }
 
-
-  private initForm(): void{
+  private initForm(): void {
     this.citaForm = this.fb.group({
-      DNI: ['', [Validators.required]],
-      name: ['', [Validators.required]],
-      lastname: ['', [Validators.required]],
+      DNI: [{ value: '', disabled: true }, [Validators.required]],
+      name: [{ value: '', disabled: true }, [Validators.required]],
+      lastname: [{ value: '', disabled: true }, [Validators.required]],
       fecha: ['', [Validators.required]],
       horario: ['', [Validators.required]],
-      codigo: ['', [Validators.required]],
-    });  
+      codigo: [{ value: '', disabled: true }, [Validators.required]],
+    });
   }
-
 }
