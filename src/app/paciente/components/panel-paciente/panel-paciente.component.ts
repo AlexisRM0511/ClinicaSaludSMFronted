0bb54e2ae8178ae919d282011896b9a08d801161
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-08-24 21:28:16 -0500

    fix: logout paciente

--- src/app/paciente/components/panel-paciente/panel-paciente.component.ts
+++ src/app/paciente/components/panel-paciente/panel-paciente.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-panel-paciente',
   templateUrl: './panel-paciente.component.html',
-  styleUrls: ['./panel-paciente.component.css']
+  styleUrls: ['./panel-paciente.component.css'],
 })
 export class PanelPacienteComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logOut() {
+    sessionStorage.clear();
+
+    this.router.navigate(['/']).then(() => {
+      window.location.reload();
+    });
   }
-
 }
