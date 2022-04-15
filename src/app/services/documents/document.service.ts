25b33ab7214e74948a40d12311f3fb9e01ea2ed8
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2022-04-15 12:05:36 -0500

    changes

--- src/app/services/documents/document.service.ts
+++ src/app/services/documents/document.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { DocumentsInterface } from 'src/app/models/documents';
 
 @Injectable({
